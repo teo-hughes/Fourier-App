5a1e25afdecd4823d77dca76c70e33fb057e0d81
TEO OLIVER HUGHES <teo_hughes@icloud.com>
2021-06-11 10:06:38 +0100

    Initial Commit

---
+++ Fourier App/Fourier_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fourier_AppApp.swift
+//  Fourier App
+//
+//  Created by Hughes, Teo (BJH) on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Fourier_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
